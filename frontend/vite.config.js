91bc662b1ba0de2efa3ddc1f648793250eb37e63
Vijayapp20 <83571252+Vijayapp20@users.noreply.github.com>
2026-08-05 11:46:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:10000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
